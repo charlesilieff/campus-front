a787fd1176750e901e328114ff5d25724b07d748
charlesilieff <95299358+charlesilieff@users.noreply.github.com>
2023-01-22 16:40:56 +0100

    minor

--- src/app/shared/util/date-utils.ts
+++ src/app/shared/util/date-utils.ts
@@ -2,11 +2,15 @@ import { APP_LOCAL_DATETIME_FORMAT } from 'app/config/constants'
 import 'dayjs/locale/fr'
 import dayjs, { Dayjs } from 'dayjs'
 dayjs.locale('fr')
-export const convertDateTimeFromServer =
-  date => (date ? dayjs(date).format(APP_LOCAL_DATETIME_FORMAT) : null)
+export const convertDateTimeFromServer = (
+  date: string | undefined
+): string => (date ? dayjs(date).format(APP_LOCAL_DATETIME_FORMAT) : null)
 
-export const convertDateTimeToServer = date => (date ? dayjs(date).toDate() : null)
+export const convertDateTimeToServer = (
+  date: string | undefined
+): Date => (date ? dayjs(date).toDate() : null)
 
-export const displayDefaultDateTime = () => dayjs().startOf('day').format(APP_LOCAL_DATETIME_FORMAT)
+export const displayDefaultDateTime = (): string =>
+  dayjs().startOf('day').format(APP_LOCAL_DATETIME_FORMAT)
 
-export const getDateKey = (date: Dayjs) => dayjs(date).format('YYYY-MM-DD')
+export const getDateKey = (date: Dayjs): string => dayjs(date).format('YYYY-MM-DD')
