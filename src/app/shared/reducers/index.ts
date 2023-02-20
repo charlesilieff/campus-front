3199db7e635542bca1d8d670305069604b92fb2f
charlesilieff <95299358+charlesilieff@users.noreply.github.com>
2023-02-20 22:52:16 +0100

    delete meal

--- src/app/shared/reducers/index.ts
+++ src/app/shared/reducers/index.ts
@@ -1,25 +1,14 @@
 /* eslint-disable simple-import-sort/imports */
 import { loadingBarReducer as loadingBar } from 'react-redux-loading-bar'
 
-// prettier-ignore
 import bed from '../../entities/bed/bed.reducer'
-// prettier-ignore
 import bedroomKind from '../../entities/bedroom-kind/bedroom-kind.reducer'
-// prettier-ignore
 import bookingBeds from '../../entities/bookingbeds/booking-beds.reducer'
-// prettier-ignore
 import customer from '../../entities/customer/customer.reducer'
-// prettier-ignore
-import meal from '../../entities/meal/meal.reducer'
-// prettier-ignore
 import place from '../../entities/place/place.reducer'
-// prettier-ignore
 import pricing from '../../entities/pricing/pricing.reducer'
-// prettier-ignore
-import reservation from '../../entities/reservation/reservation.reducer'
-// prettier-ignore
 import requestReservation from '../../entities/reservation-request/reservation-request.reducer'
-// prettier-ignore
+import reservation from '../../entities/reservation/reservation.reducer'
 import room from '../../entities/room/room.reducer'
 import activate from '../../modules/account/activate/activate.reducer'
 import passwordReset from '../../modules/account/password-reset/password-reset.reducer'
@@ -30,7 +19,6 @@ import administration from '../../modules/administration/administration.reducer'
 import userManagement from '../../modules/administration/user-management/user-management.reducer'
 import applicationProfile from './application-profile'
 import authentication from './authentication'
-/* jhipster-needle-add-reducer-import - JHipster will add reducer here */
 
 export const rootReducer = {
   authentication,
@@ -51,7 +39,6 @@ export const rootReducer = {
   requestReservation,
   bookingBeds,
   place,
-  meal,
   /* jhipster-needle-add-reducer-combine - JHipster will add reducer here */
   loadingBar
 }
