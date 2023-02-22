a3f87c49e68155f06584f7e4121d000f17c7aac7
charlesilieff <95299358+charlesilieff@users.noreply.github.com>
2023-02-22 10:50:57 +0100

    room

--- src/app/entities/room/room.tsx
+++ src/app/entities/room/room.tsx
@@ -1,11 +1,9 @@
-import { Heading } from '@chakra-ui/react'
-import { faEye, faPencilAlt, faPlus, faSync } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Button, Heading, HStack, Table, Tbody, Td, Th, Thead, Tr, VStack } from '@chakra-ui/react'
 import { useAppDispatch, useAppSelector } from 'app/config/store'
 import { PlaceMenu } from 'app/shared/layout/menus/placeMenu'
 import React, { useEffect } from 'react'
+import { FaEye, FaPencilAlt, FaPlus, FaSync } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
-import { Button, Table } from 'reactstrap'
 
 import { getEntities } from './room.reducer'
 import { RoomDeleteDialog } from './roomdelete-dialog'
@@ -25,96 +23,98 @@ export const Room = () => {
   }
 
   return (
-    <div>
-      <h2 id="room-heading" data-cy="RoomHeading">
-        <Heading>Chambres / Lieux de couchage</Heading>
-        <div className="d-flex justify-content-end">
-          <Button className="mr-2" color="info" onClick={handleSyncList} disabled={loading}>
-            <FontAwesomeIcon icon={faSync} spin={loading} /> Rafraîchir
-          </Button>
-          <Link
-            to={`room/new`}
-            className="btn btn-primary jh-create-entity"
-            id="jh-create-entity"
-            data-cy="entityCreateButton"
+    <VStack>
+      <Heading>Chambres / Lieux de couchage</Heading>
+      <HStack justifyContent={'space-between'} w={'100%'}>
+        <PlaceMenu />
+        <HStack>
+          <Button
+            variant={'see'}
+            onClick={handleSyncList}
+            isLoading={loading}
+            leftIcon={<FaSync />}
           >
-            <FontAwesomeIcon icon={faPlus} />
-            &nbsp; Créer une chambre
-          </Link>
-        </div>
-      </h2>
-      <PlaceMenu />
-      <div className="table-responsive">
-        {roomList && roomList.length > 0 ?
-          (
-            <Table responsive>
-              <thead>
-                <tr>
-                  <th>Nom / Numéro</th>
-                  <th>Commentaire</th>
-                  <th>Type de chambre</th>
-                  <th>Emplacement de la chambre</th>
-                  <th>Nombre de lits</th>
-                  <th />
-                </tr>
-              </thead>
-              <tbody>
-                {roomList.map((room, i) => (
-                  <tr key={`entity-${i}`} data-cy="entityTable">
-                    <td>
-                      {' '}
-                      <Button tag={Link} to={`${room.id}`} color="link" size="sm">
-                        {room.name}
-                      </Button>
-                    </td>
-                    <td>{room.comment}</td>
-                    <td>
-                      {room.bedroomKind ?
-                        (
-                          <Link to={`/bedroom-kind/${room.bedroomKind.id}`}>
-                            {room.bedroomKind.name}
-                          </Link>
-                        ) :
-                        ''}
-                    </td>
-                    <td>
-                      {room.place ?
-                        <Link to={`/place/${room.place.id}`}>{room.place.name}</Link> :
-                        ''}
-                    </td>
-                    <td>{room.beds?.length}</td>
-                    <td className="text-right">
-                      <div className="btn-group flex-btn-group-container">
-                        <Button
-                          tag={Link}
-                          to={`${room.id}`}
-                          color="info"
-                          size="sm"
-                          data-cy="entityDetailsButton"
-                        >
-                          <FontAwesomeIcon icon={faEye} />{' '}
-                          <span className="d-none d-md-inline">Voir</span>
-                        </Button>
-                        <Button
-                          tag={Link}
-                          to={`${room.id}/edit`}
-                          color="primary"
-                          size="sm"
-                          data-cy="entityEditButton"
-                        >
-                          <FontAwesomeIcon icon={faPencilAlt} />{' '}
-                          <span className="d-none d-md-inline">Éditer</span>
-                        </Button>
-                        <RoomDeleteDialog roomId={room.id} />
-                      </div>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </Table>
-          ) :
-          (!loading && <div className="alert alert-warning">Pas de chambres crées.</div>)}
-      </div>
-    </div>
+            Rafraîchir
+          </Button>
+          <Button
+            as={Link}
+            color={'white'}
+            backgroundColor={'#e95420'}
+            _hover={{ textDecoration: 'none', color: 'orange' }}
+            to={`new`}
+            leftIcon={<FaPlus />}
+          >
+            Créer une chambre
+          </Button>
+        </HStack>
+      </HStack>
+
+      {roomList && roomList.length > 0 ?
+        (
+          <Table>
+            <Thead>
+              <Tr>
+                <Th>Nom / Numéro</Th>
+                <Th>Commentaire</Th>
+                <Th>Type de chambre</Th>
+                <Th>Emplacement de la chambre</Th>
+                <Th>Nombre de lits</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {roomList.map((room, i) => (
+                <Tr key={`entity-${i}`} data-cy="entityTable">
+                  <Td>
+                    {' '}
+                    <Button as={Link} to={`${room.id}`} variant="see" size="sm">
+                      {room.name}
+                    </Button>
+                  </Td>
+                  <Td>{room.comment}</Td>
+                  <Td>
+                    {room.bedroomKind ?
+                      (
+                        <Link to={`/bedroom-kind/${room.bedroomKind.id}`}>
+                          {room.bedroomKind.name}
+                        </Link>
+                      ) :
+                      ''}
+                  </Td>
+                  <Td>
+                    {room.place ?
+                      <Link to={`/place/${room.place.id}`}>{room.place.name}</Link> :
+                      ''}
+                  </Td>
+                  <Td>{room.beds?.length}</Td>
+                  <Td className="text-right">
+                    <Button
+                      as={Link}
+                      to={`${room.id}`}
+                      variant={'see'}
+                      leftIcon={<FaEye />}
+                      size="sm"
+                      borderRightRadius={0}
+                    >
+                      Voir
+                    </Button>
+                    <Button
+                      as={Link}
+                      to={`${room.id}/edit`}
+                      variant={'modify'}
+                      leftIcon={<FaPencilAlt />}
+                      size="sm"
+                      borderRadius={0}
+                    >
+                      Éditer
+                    </Button>
+                    <RoomDeleteDialog roomId={room.id} />
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        ) :
+        (!loading && <div className="alert alert-warning">Pas de chambres crées.</div>)}
+    </VStack>
   )
 }
