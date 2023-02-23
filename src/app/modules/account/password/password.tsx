6b817a3c83bf2e47c98a54906513b2646f1e23a2
charlesilieff <95299358+charlesilieff@users.noreply.github.com>
2023-02-23 09:17:30 +0100

    new password

--- src/app/modules/account/password/password.tsx
+++ src/app/modules/account/password/password.tsx
@@ -1,11 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
+import { Button, Heading, VStack } from '@chakra-ui/react'
 import { useAppDispatch, useAppSelector } from 'app/config/store'
 import { PasswordStrengthBar } from 'app/shared/layout/password/password-strength-bar'
 import { getSession } from 'app/shared/reducers/authentication'
 import React, { useEffect, useState } from 'react'
+import { FaSave } from 'react-icons/fa'
 import { ValidatedField, ValidatedForm } from 'react-jhipster'
 import { toast } from 'react-toastify'
-import { Button, Col, Row } from 'reactstrap'
 
 import { reset, savePassword } from './password.reducer'
 
@@ -41,66 +42,64 @@ export const Password = () => {
   }, [successMessage, errorMessage])
 
   return (
-    <div>
-      <Row className="justify-content-center">
-        <Col md="8">
-          <h2 id="password-title">Password for {account.login}</h2>
-          <ValidatedForm id="password-form" onSubmit={handleValidSubmit}>
-            <ValidatedField
-              name="currentPassword"
-              label="Current password"
-              placeholder={'Current password'}
-              type="password"
-              validate={{
-                required: { value: true, message: 'Your password is required.' }
-              }}
-              data-cy="currentPassword"
-            />
-            <ValidatedField
-              name="newPassword"
-              label="New password"
-              placeholder={'New password'}
-              type="password"
-              validate={{
-                required: { value: true, message: 'Your password is required.' },
-                minLength: {
-                  value: 4,
-                  message: 'Your password is required to be at least 4 characters.'
-                },
-                maxLength: {
-                  value: 50,
-                  message: 'Your password cannot be longer than 50 characters.'
-                }
-              }}
-              onChange={updatePassword}
-              data-cy="newPassword"
-            />
-            <PasswordStrengthBar password={password} />
-            <ValidatedField
-              name="confirmPassword"
-              label="New password confirmation"
-              placeholder="Confirm the new password"
-              type="password"
-              validate={{
-                required: { value: true, message: 'Your confirmation password is required.' },
-                minLength: {
-                  value: 4,
-                  message: 'Your confirmation password is required to be at least 4 characters.'
-                },
-                maxLength: {
-                  value: 50,
-                  message: 'Your confirmation password cannot be longer than 50 characters.'
-                },
-                validate: v => v === password || 'The password and its confirmation do not match!'
-              }}
-              data-cy="confirmPassword"
-            />
-            <Button color="success" type="submit" data-cy="submit">
-              Save
-            </Button>
-          </ValidatedForm>
-        </Col>
-      </Row>
-    </div>
+    <VStack>
+      <Heading>Mot de passe pour {account.login}</Heading>
+      <VStack p={8}>
+        <ValidatedForm id="password-form" onSubmit={handleValidSubmit}>
+          <ValidatedField
+            name="currentPassword"
+            label="Mot de passe actuel"
+            placeholder={'Mot de passe actuel'}
+            type="password"
+            validate={{
+              required: { value: true, message: 'Your password is required.' }
+            }}
+            data-cy="currentPassword"
+          />
+          <ValidatedField
+            name="newPassword"
+            label="Nouveau mot de passe"
+            placeholder={'Nouveau mot de passe'}
+            type="password"
+            validate={{
+              required: { value: true, message: 'Your password is required.' },
+              minLength: {
+                value: 4,
+                message: 'Your password is required to be at least 4 characters.'
+              },
+              maxLength: {
+                value: 50,
+                message: 'Your password cannot be longer than 50 characters.'
+              }
+            }}
+            onChange={updatePassword}
+            data-cy="newPassword"
+          />
+          <PasswordStrengthBar password={password} />
+          <ValidatedField
+            name="confirmPassword"
+            label="Confirmation du nouveau mot de passe"
+            placeholder="Confirmation du nouveau mot de passe"
+            type="password"
+            validate={{
+              required: { value: true, message: 'Your confirmation password is required.' },
+              minLength: {
+                value: 4,
+                message: 'Your confirmation password is required to be at least 4 characters.'
+              },
+              maxLength: {
+                value: 50,
+                message: 'Your confirmation password cannot be longer than 50 characters.'
+              },
+              validate: v => v === password || 'The password and its confirmation do not match!'
+            }}
+            data-cy="confirmPassword"
+          />
+          <Button variant={'save'} type="submit" leftIcon={<FaSave />}>
+            Enregistrer
+          </Button>
+        </ValidatedForm>
+      </VStack>
+    </VStack>
   )
 }
