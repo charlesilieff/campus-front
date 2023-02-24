c4460e6504ea4ef1071c0315b6b820b7b89e8dc6
charlesilieff <95299358+charlesilieff@users.noreply.github.com>
2023-02-24 14:01:24 +0100

    rgpd

--- src/app/modules/administration/user-management/user-management-update.tsx
+++ src/app/modules/administration/user-management/user-management-update.tsx
@@ -1,12 +1,10 @@
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons/faArrowLeft'
-import { faSave } from '@fortawesome/free-solid-svg-icons/faSave'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Button, Heading, HStack, VStack } from '@chakra-ui/react'
 import { useAppDispatch, useAppSelector } from 'app/config/store'
 import type { IUser } from 'app/shared/model/user.model'
 import React, { useEffect } from 'react'
+import { FaArrowLeft, FaSave } from 'react-icons/fa'
 import { isEmail, ValidatedField, ValidatedForm } from 'react-jhipster'
 import { Link, useNavigate, useParams } from 'react-router-dom'
-import { Button, Col, FormText, Row } from 'reactstrap'
 
 import { createUser, getRoles, getUser, reset, updateUser } from './user-management.reducer'
 
@@ -51,125 +49,115 @@ export const UserManagementUpdate = () => {
   const authorities = useAppSelector(state => state.userManagement.authorities)
 
   return (
-    <div>
-      <Row className="justify-content-center">
-        <Col md="8">
-          <h1>Créer ou éditer un utilisateur</h1>
-        </Col>
-      </Row>
-      <Row className="justify-content-center">
-        <Col md="8">
-          {loading ? <p>Loading...</p> : (
-            <ValidatedForm onSubmit={saveUser} defaultValues={user}>
-              {user.id ?
-                (
-                  <ValidatedField
-                    type="text"
-                    name="id"
-                    required
-                    readOnly
-                    label="ID"
-                    validate={{ required: true }}
-                  />
-                ) :
-                null}
-              <ValidatedField
-                type="text"
-                name="login"
-                label="Login"
-                validate={{
-                  required: {
-                    value: true,
-                    message: "Votre nom d'utilisateur est obligatoire."
-                  },
-                  pattern: {
-                    value:
-                      /^[a-zA-Z0-9!$&*+=?^_`{|}~.-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$|^[_.@A-Za-z0-9-]+$/,
-                    message: "Votre nom d'utilisateur est invalide."
-                  },
-                  minLength: {
-                    value: 1,
-                    message: "Votre nom d'utilisateur doit contenir plus d'un caractère."
-                  },
-                  maxLength: {
-                    value: 50,
-                    message: "Votre nom d'utilisateur ne peut pas contenir plus de 50 caractères."
-                  }
-                }}
-              />
-              <ValidatedField
-                type="text"
-                name="firstName"
-                label="Prénom"
-                validate={{
-                  maxLength: {
-                    value: 50,
-                    message: 'Ce champ doit faire moins de 50 caractères.'
-                  }
-                }}
-              />
-              <ValidatedField
-                type="text"
-                name="lastName"
-                label="Nom"
-                validate={{
-                  maxLength: {
-                    value: 50,
-                    message: 'Ce champ doit faire moins de 50 caractères.'
-                  }
-                }}
-              />
-              <FormText>This field cannot be longer than 50 characters.</FormText>
-              <ValidatedField
-                name="email"
-                label="Email"
-                placeholder="Votre email"
-                type="email"
-                validate={{
-                  required: {
-                    value: true,
-                    message: 'Votre email est requis.'
-                  },
-                  minLength: {
-                    value: 5,
-                    message: 'Votre email doit comporter au moins 5 caractères.'
-                  },
-                  maxLength: {
-                    value: 254,
-                    message: 'Votre email ne doit pas comporter plus de 50 caractères.'
-                  },
-                  validate: v => isEmail(v) || "Votre email n'est pas valide."
-                }}
-              />
-              <ValidatedField
-                type="checkbox"
-                name="activated"
-                check
-                value={true}
-                disabled={!user.id}
-                label="Activé"
-              />
-              <ValidatedField type="select" name="authorities" multiple label="Droits">
-                {authorities.map(role => (
-                  <option value={role} key={role}>
-                    {role}
-                  </option>
-                ))}
-              </ValidatedField>
-              <Button tag={Link} to="/admin/user-management" replace color="info">
-                <FontAwesomeIcon icon={faArrowLeft} />
-                &nbsp;
-                <span className="d-none d-md-inline">Retour</span>
-              </Button>
-              &nbsp;
-              <Button color="primary" type="submit" disabled={isInvalid || updating}>
-                <FontAwesomeIcon icon={faSave} />
-                &nbsp; Sauvegarder
-              </Button>
-            </ValidatedForm>
-          )}
-        </Col>
-      </Row>
-    </div>
+    <VStack>
+      <Heading>Créer ou éditer un utilisateur</Heading>
+
+      {loading ? <p>Loading...</p> : (
+        <ValidatedForm onSubmit={saveUser} defaultValues={user}>
+          <ValidatedField
+            type="text"
+            name="login"
+            label="Login"
+            validate={{
+              required: {
+                value: true,
+                message: "Votre nom d'utilisateur est obligatoire."
+              },
+              pattern: {
+                value:
+                  /^[a-zA-Z0-9!$&*+=?^_`{|}~.-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$|^[_.@A-Za-z0-9-]+$/,
+                message: "Votre nom d'utilisateur est invalide."
+              },
+              minLength: {
+                value: 1,
+                message: "Votre nom d'utilisateur doit contenir plus d'un caractère."
+              },
+              maxLength: {
+                value: 50,
+                message: "Votre nom d'utilisateur ne peut pas contenir plus de 50 caractères."
+              }
+            }}
+          />
+          <ValidatedField
+            type="text"
+            name="firstName"
+            label="Prénom"
+            validate={{
+              maxLength: {
+                value: 50,
+                message: 'Ce champ doit faire moins de 50 caractères.'
+              }
+            }}
+          />
+          <ValidatedField
+            type="text"
+            name="lastName"
+            label="Nom"
+            validate={{
+              maxLength: {
+                value: 50,
+                message: 'Ce champ doit faire moins de 50 caractères.'
+              }
+            }}
+          />
+
+          <ValidatedField
+            name="email"
+            label="Email"
+            placeholder="Votre email"
+            type="email"
+            validate={{
+              required: {
+                value: true,
+                message: 'Votre email est requis.'
+              },
+              minLength: {
+                value: 5,
+                message: 'Votre email doit comporter au moins 5 caractères.'
+              },
+              maxLength: {
+                value: 254,
+                message: 'Votre email ne doit pas comporter plus de 50 caractères.'
+              },
+              validate: v => isEmail(v) || "Votre email n'est pas valide."
+            }}
+          />
+          <ValidatedField
+            type="checkbox"
+            name="activated"
+            check
+            value={true}
+            disabled={!user.id}
+            label="Activé"
+          />
+          <ValidatedField type="select" name="authorities" multiple label="Droits">
+            {authorities.map(role => (
+              <option value={role} key={role}>
+                {role}
+              </option>
+            ))}
+          </ValidatedField>
+          <HStack>
+            <Button
+              as={Link}
+              to="/admin/user-management"
+              variant={'back'}
+              leftIcon={<FaArrowLeft />}
+            >
+              Retour
+            </Button>
+
+            <Button
+              variant="save"
+              type="submit"
+              disabled={isInvalid || updating}
+              leftIcon={<FaSave />}
+            >
+              Sauvegarder
+            </Button>
+          </HStack>
+        </ValidatedForm>
+      )}
+    </VStack>
   )
 }
--- src/app/modules/rgpd/rgpdAnonymizeData.tsx
+++ src/app/modules/rgpd/rgpdAnonymizeData.tsx
@@ -1,13 +1,14 @@
-import { faTrash } from '@fortawesome/free-solid-svg-icons/faTrash'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Button, HStack, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, ModalOverlay,
+  useDisclosure } from '@chakra-ui/react'
 import type { ICustomer } from 'app/shared/model/customer.model'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap'
+import { FaBan, FaTrash } from 'react-icons/fa'
 
 export const RGPDAnonymizeData = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
   const [isCustomersToAnonymize, setIsCustomersToAnonymize] = useState(false)
-  const [isOpen, setIsOpen] = useState(false)
+
   const [customers, setCustomers] = useState([] as ICustomer[])
   const requestUrl = `/api/anonymizecustomers`
 
@@ -35,10 +36,10 @@ export const RGPDAnonymizeData = () => {
       await axios.put<ICustomer[]>(requestUrl, customers)
 
       alert('Les données ont bien été anonymisées')
-      setIsOpen(false)
+      onClose()
     } catch (error) {
       alert(`Une erreur s'est produite : ${error}`)
-      setIsOpen(false)
+      onClose()
     }
   }
 
@@ -48,45 +49,45 @@ export const RGPDAnonymizeData = () => {
 
   return (
     <>
-      <Modal isOpen={isOpen}>
-        <ModalHeader toggle={() => setIsOpen(false)} data-cy="bedDeleteDialogHeading">
-          Confirmer l&apos;opération.
-        </ModalHeader>
-        <ModalBody id="gestionhebergementApp.bed.delete.question">
-          Êtes-vous sûr de vouloir anonymiser les données clients ?
-        </ModalBody>
-        <ModalFooter>
-          <Button color="secondary" onClick={() => setIsOpen(false)}>
-            <FontAwesomeIcon icon="ban" />
-            &nbsp; Retour
-          </Button>
-          <Button
-            id="jhi-confirm-delete-bed"
-            data-cy="entityConfirmDeleteButton"
-            color="danger"
-            onClick={() => anonymizeCustomers()}
-          >
-            <FontAwesomeIcon icon={faTrash} />
-            &nbsp; Confirmer
-          </Button>
-        </ModalFooter>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>
+            Confirmer l&apos;opération.
+          </ModalHeader>
+          <ModalBody id="gestionhebergementApp.bed.delete.question">
+            Êtes-vous sûr de vouloir anonymiser les données clients ?
+          </ModalBody>
+          <ModalFooter>
+            <HStack>
+              <Button variant="back" onClick={onClose} leftIcon={<FaBan />}>
+                Retour
+              </Button>
+              <Button
+                variant="danger"
+                onClick={anonymizeCustomers}
+                leftIcon={<FaTrash />}
+              >
+                Confirmer
+              </Button>
+            </HStack>
+          </ModalFooter>
+        </ModalContent>
       </Modal>
 
       {isCustomersToAnonymize ?
         (
           <Button
-            id="delete-rgpd-data"
-            data-cy="deleteLink"
-            color="danger"
-            onClick={() => setIsOpen(true)}
+            variant="danger"
+            onClick={onOpen}
+            leftIcon={<FaTrash />}
           >
-            <FontAwesomeIcon icon={faTrash} />
-            &nbsp; Anonymiser les données clients.
+            Anonymiser les données clients.
           </Button>
         ) :
         (
           <div>
-            RGPD : Pas de données clients à anonymiser (derniére réservation antérieure à 3 ans).
+            RGPD : Pas de données clients à anonymiser (dernière réservation antérieure à 3 ans).
           </div>
         )}
     </>
