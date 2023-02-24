8d6a956df0bc0735e8e7aa4bc221e9b05f4cca03
charlesilieff <95299358+charlesilieff@users.noreply.github.com>
2023-02-24 15:12:09 +0100

    log in

--- src/app/modules/login/login-modal.tsx
+++ src/app/modules/login/login-modal.tsx
@@ -1,9 +1,30 @@
-import { Button } from '@chakra-ui/react'
+import {
+  Alert,
+  Button,
+  Checkbox,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  HStack,
+  Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  VStack
+} from '@chakra-ui/react'
 import React from 'react'
 import { useForm } from 'react-hook-form'
-import { ValidatedField } from 'react-jhipster'
 import { Link } from 'react-router-dom'
-import { Alert, Col, Form, Modal, ModalBody, ModalFooter, ModalHeader, Row } from 'reactstrap'
+
+interface UserForm {
+  username: string
+  password: string
+  rememberMe: boolean
+}
 
 export interface ILoginModalProps {
   showModal: boolean
@@ -22,96 +43,113 @@ export const LoginModal = (props: ILoginModalProps) => {
   const {
     handleSubmit,
     register,
-    formState: { errors, touchedFields }
-  } = useForm({ mode: 'onTouched' })
+    formState: { errors }
+  } = useForm<UserForm>({ mode: 'onTouched' })
 
   const { loginError, handleClose } = props
 
   return (
     <Modal
       isOpen={props.showModal}
-      toggle={handleClose}
-      backdrop="static"
-      id="login-page"
+      onClose={handleClose}
       autoFocus={false}
+      size="xl"
     >
-      <Form onSubmit={handleSubmit(login)}>
-        <ModalHeader id="login-title" data-cy="loginTitle" toggle={handleClose}>
-          Connexion
-        </ModalHeader>
-        <ModalBody>
-          <Row>
-            <Col md="12">
+      <ModalOverlay />
+      <ModalContent>
+        <form
+          onSubmit={handleSubmit(login)}
+        >
+          <ModalHeader id="login-title" data-cy="loginTitle">
+            Connexion
+          </ModalHeader>
+          <ModalBody>
+            <VStack spacing={4}>
               {loginError ?
                 (
-                  <Alert color="danger" data-cy="loginError">
-                    <strong>Impossible de se connecter to sign in!</strong>{' '}
+                  <Alert status="error" data-cy="loginError">
+                    <strong>Impossible de se connecter !</strong>{' '}
                     Vérifier votre mot de passe et/ou votre nom d&apos;utilisateur.
                   </Alert>
                 ) :
                 null}
-            </Col>
-            <Col md="12">
-              <ValidatedField
-                name="username"
-                label="Nom d'utilisateur"
-                placeholder="Votre nom d'utilisateur"
-                required
-                autoFocus
-                data-cy="username"
-                validate={{
-                  required: "Le nom d'utilisateur ne peut pas être vide."
-                }}
-                register={register}
-                // @ts-expect-error react-hook-form types are not up to date
-                error={errors.username}
-                isTouched={touchedFields.username}
-              />
-              <ValidatedField
-                name="password"
-                type="password"
-                label="Mot de passe"
-                placeholder="Mot de passe"
-                required
-                data-cy="password"
-                validate={{
-                  required: 'Le mot de passe ne peut pas être vide.'
-                }}
-                register={register}
-                // @ts-expect-error react-hook-form types are not up to date
-                error={errors.password}
-                isTouched={touchedFields.password}
-              />
-              <ValidatedField
-                name="rememberMe"
-                type="checkbox"
-                check
-                label="Souvenez-vous de moi"
-                value={true}
-                register={register}
-              />
-            </Col>
-          </Row>
-          <div className="mt-1">&nbsp;</div>
-          <Alert color="warning">
-            <Link to="/account/reset/request" data-cy="forgetYourPasswordSelector">
-              Avez-vous oublié votre mot de passe ?
-            </Link>
-          </Alert>
-          <Alert color="warning">
-            <span>Vous n&apos;avez pas encore de compte ?</span>{' '}
-            <Link to="/account/register">Créer un compte.</Link>
-          </Alert>
-        </ModalBody>
-        <ModalFooter>
-          <Button color="secondary" onClick={handleClose} tabIndex={1}>
-            Annuler
-          </Button>{' '}
-          <Button backgroundColor={'#17a2b8'} color={'white'} type="submit" data-cy="submit">
-            Se connecter
-          </Button>
-        </ModalFooter>
-      </Form>
+
+              <FormControl isRequired isInvalid={errors.username !== undefined}>
+                <FormLabel htmlFor="username" fontWeight={'bold'}>
+                  {"Nom d'utilisateur"}
+                </FormLabel>
+                <Input
+                  id="username"
+                  type="text"
+                  placeholder="Nom d'utilisateur"
+                  {...register('username', {
+                    required: "Le nom d'utilisateur est obligatoire"
+                  })}
+                />
+
+                <FormErrorMessage>
+                  {errors.username && errors.username.message}
+                </FormErrorMessage>
+              </FormControl>
+              <FormControl isRequired isInvalid={errors.password !== undefined}>
+                <FormLabel htmlFor="password" fontWeight={'bold'}>
+                  {'Mot de passe'}
+                </FormLabel>
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="Mot de passe"
+                  {...register('password', {
+                    required: 'Le mot de passe ne peut pas être vide.'
+                  })}
+                />
+
+                <FormErrorMessage>
+                  {errors.password && errors.password.message}
+                </FormErrorMessage>
+              </FormControl>
+              <FormControl>
+                <HStack>
+                  <Text fontWeight={'bold'}>{'Souvenez-vous de moi'}</Text>
+                  <Checkbox
+                    id="rememberMe"
+                    placeholder="Type"
+                    {...register('rememberMe')}
+                  />
+                </HStack>
+
+                <FormErrorMessage>
+                  {errors.password && errors.password.message}
+                </FormErrorMessage>
+              </FormControl>
+
+              <VStack w="100%">
+                <Alert status="warning">
+                  <Link to="/account/reset/request" data-cy="forgetYourPasswordSelector">
+                    Avez-vous oublié votre mot de passe ?
+                  </Link>
+                </Alert>
+                <Alert status="warning">
+                  <HStack>
+                    <span>Vous n&apos;avez pas encore de compte ?</span>{' '}
+                    <Link to="/account/register">Créer un compte.</Link>
+                  </HStack>
+                </Alert>
+              </VStack>
+            </VStack>
+          </ModalBody>
+          <ModalFooter>
+            <HStack>
+              <Button variant="back" onClick={handleClose} tabIndex={1}>
+                Annuler
+              </Button>
+              <Button variant="save" type="submit" data-cy="submit">
+                Se connecter
+              </Button>
+            </HStack>
+          </ModalFooter>
+        </form>
+      </ModalContent>
     </Modal>
   )
 }
