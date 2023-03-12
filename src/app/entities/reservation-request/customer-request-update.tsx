0c57dd457effc7a3aab147bd1ebe5b3f650e1199
charlesilieff <95299358+charlesilieff@users.noreply.github.com>
2023-03-12 16:03:15 +0100

    jhipster less

--- src/app/entities/reservation-request/customer-request-update.tsx
+++ src/app/entities/reservation-request/customer-request-update.tsx
@@ -1,20 +1,38 @@
-import { Button, Heading, VStack } from '@chakra-ui/react'
+import {
+  Button,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Heading,
+  Input,
+  VStack
+} from '@chakra-ui/react'
 import { useAppDispatch, useAppSelector } from 'app/config/store'
 import type { ICustomer } from 'app/shared/model/customer.model'
 import React, { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
 import { FaSave } from 'react-icons/fa'
-import { isNumber, ValidatedField, ValidatedForm } from 'react-jhipster'
 import { useParams } from 'react-router-dom'
 
 import { getEntity, reset, setData } from './reservation-request.reducer'
 
 export const CustomerUpdate = () => {
+  const customerEntity = useAppSelector(state => state.requestReservation.entity.customer)
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    reset: resetForm
+  } = useForm<ICustomer>({})
+
+  useEffect(() => {
+    resetForm(defaultValues())
+  }, [customerEntity?.id])
   const dispatch = useAppDispatch()
   const { uuid } = useParams<'uuid'>()
 
   const isNew = uuid === undefined
 
-  const customerEntity = useAppSelector(state => state.requestReservation.entity.customer)
   const loading = useAppSelector(state => state.requestReservation.loading)
 
   useEffect(() => {
@@ -45,86 +63,117 @@ export const CustomerUpdate = () => {
       <Heading size={'md'}>
         {isNew ? 'Créez' : 'Modifiez'} votre demande de réservation : informations de contact
       </Heading>
-
       {loading ? <p>Chargement...</p> : (
-        <ValidatedForm
-          defaultValues={defaultValues()}
-          onSubmit={e => saveCustomer(e)}
-        >
-          <ValidatedField
-            label="Prénom"
-            id="customer-firstname"
-            name="firstname"
-            data-cy="firstname"
-            type="text"
-            validate={{
-              required: { value: true, message: 'Ce champ est obligatoire.' },
-              minLength: {
-                value: 1,
-                message: 'This field is required to be at least 1 characters.'
-              },
-              maxLength: {
-                value: 50,
-                message: 'This field cannot be longer than 50 characters.'
-              }
-            }}
-          />
-          <ValidatedField
-            label="Nom"
-            id="customer-lastname"
-            name="lastname"
-            data-cy="lastname"
-            type="text"
-            validate={{
-              required: { value: true, message: 'Ce champ est obligatoire.' },
-              minLength: { value: 1, message: 'Minimum 1' },
-              maxLength: { value: 50, message: 'Maximum 50' }
-            }}
-          />
-          <ValidatedField
-            label="Age"
-            id="customer-age"
-            name="age"
-            data-cy="age"
-            type="number"
-            validate={{
-              min: { value: 1, message: 'Minimum 1' },
-              max: { value: 125, message: 'Maximum 125' },
-              validate: v => isNumber(v) || 'Ce doit être un nombre.'
-            }}
-          />
+        <form onSubmit={handleSubmit(saveCustomer)}>
+          <VStack minW={'300px'}>
+            <FormControl isRequired isInvalid={errors.lastname !== undefined}>
+              <FormLabel htmlFor="firstname" fontWeight={'bold'}>
+                {'Prénom'}
+              </FormLabel>
+              <Input
+                id="firstname"
+                type="text"
+                placeholder="Prénom"
+                {...register('firstname', {
+                  required: 'Le prénom est obligatoire',
+                  minLength: {
+                    value: 1,
+                    message: 'This field is required to be at least 1 characters.'
+                  },
+                  maxLength: {
+                    value: 50,
+                    message: 'This field cannot be longer than 50 characters.'
+                  }
+                })}
+              />
 
-          <ValidatedField
-            label="Téléphone"
-            id="customer-phoneNumber"
-            name="phoneNumber"
-            data-cy="phoneNumber"
-            type="tel"
-            validate={{
-              required: { value: true, message: 'Le numéro de téléphone est obligatoire.' },
-              minLength: { value: 10, message: 'Minimum 10' },
-              maxLength: { value: 16, message: 'Maximum 16' }
-            }}
-          />
-          <ValidatedField
-            label="Email"
-            id="customer-email"
-            name="email"
-            data-cy="email"
-            type="email"
-            validate={{
-              required: { value: true, message: "L'adresse email est obligatoire." },
-              pattern: {
-                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                message: 'Adresse email invalide.'
-              }
-            }}
-          />
-          &nbsp;
-          <Button variant={'save'} type="submit" leftIcon={<FaSave />}>
-            Suivant
-          </Button>
-        </ValidatedForm>
+              <FormErrorMessage>
+                {errors.firstname && errors.firstname.message}
+              </FormErrorMessage>
+            </FormControl>
+            <FormControl isRequired isInvalid={errors.lastname !== undefined}>
+              <FormLabel htmlFor="lastname" fontWeight={'bold'}>
+                {'Nom'}
+              </FormLabel>
+              <Input
+                type="text"
+                placeholder="Nom"
+                {...register('lastname', {
+                  required: 'Le nom est obligatoire',
+                  minLength: {
+                    value: 1,
+                    message: 'This field is required to be at least 1 characters.'
+                  },
+                  maxLength: {
+                    value: 50,
+                    message: 'This field cannot be longer than 50 characters.'
+                  }
+                })}
+              />
+
+              <FormErrorMessage>
+                {errors.lastname && errors.lastname.message}
+              </FormErrorMessage>
+            </FormControl>
+            <FormControl isInvalid={errors.age !== undefined}>
+              <FormLabel htmlFor="age" fontWeight={'bold'}>
+                {'Age'}
+              </FormLabel>
+              <Input
+                type="number"
+                placeholder="Age"
+                {...register('age', {})}
+              />
+
+              <FormErrorMessage>
+                {errors.age && errors.age.message}
+              </FormErrorMessage>
+            </FormControl>
+            <FormControl isRequired isInvalid={errors.phoneNumber !== undefined}>
+              <FormLabel htmlFor="phoneNumber" fontWeight={'bold'}>
+                {'Téléphone'}
+              </FormLabel>
+              <Input
+                type="text"
+                placeholder="Téléphone"
+                {...register('phoneNumber', {})}
+              />
+
+              <FormErrorMessage>
+                {errors.phoneNumber && errors.phoneNumber.message}
+              </FormErrorMessage>
+            </FormControl>
+            <FormControl isRequired isInvalid={errors.email !== undefined}>
+              <FormLabel htmlFor="email" fontWeight={'bold'}>
+                {'Email'}
+              </FormLabel>
+              <Input
+                id="email"
+                type="email"
+                placeholder="Email"
+                {...register('email', {
+                  required: "L'email est obligatoire",
+                  minLength: {
+                    value: 1,
+                    message: 'This field is required to be at least 1 characters.'
+                  },
+                  maxLength: {
+                    value: 50,
+                    message: 'This field cannot be longer than 50 characters.'
+                  }
+                })}
+              />
+
+              <FormErrorMessage>
+                {errors.email && errors.email.message}
+              </FormErrorMessage>
+            </FormControl>
+
+            <Button variant={'save'} type="submit" leftIcon={<FaSave />}>
+              Suivant
+            </Button>
+          </VStack>
+        </form>
       )}
     </VStack>
   )
