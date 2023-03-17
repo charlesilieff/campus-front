fa0003bbdb777b302e9e35b03c02724df913c7b3
Gurun <gurunlg@hotmail.com>
2023-03-17 09:54:52 +0100

    phone not required on public reservation

--- src/app/entities/reservation-request/customer-request-update.tsx
+++ src/app/entities/reservation-request/customer-request-update.tsx
@@ -129,7 +129,7 @@ export const CustomerUpdate = () => {
                 {errors.age && errors.age.message}
               </FormErrorMessage>
             </FormControl>
-            <FormControl isRequired isInvalid={errors.phoneNumber !== undefined}>
+            <FormControl isInvalid={errors.phoneNumber !== undefined}>
               <FormLabel htmlFor="phoneNumber" fontWeight={'bold'}>
                 {'Téléphone'}
               </FormLabel>
---
+++ src/app/modules/reservation-to-be-processed/reservationtest.tsx
@@ -0,0 +1,95 @@
+import { Box, Button, HStack, Spinner, Text, VStack } from '@chakra-ui/react'
+import * as O from '@effect/data/Option'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { BsFillExclamationCircleFill } from 'react-icons/bs'
+import { Link } from 'react-router-dom'
+
+interface ReservationToBeProcessed {
+  total: number
+  urgent: number
+}
+
+export const ReservationsToBeProcessed = () => {
+  const [reservationToBeProcessed, setReservationToBeProcessed] = useState<
+    O.Option<ReservationToBeProcessed>
+  >(O.none)
+
+  useEffect(() => {
+    const fetchReservationsToBeProcessed = async () => {
+      const requestUrl = 'api/reservation-to-be-processed-count'
+      const { data } = await axios.get<ReservationToBeProcessed>(requestUrl)
+      setReservationToBeProcessed(O.some(data))
+    }
+    fetchReservationsToBeProcessed()
+  }, [])
+
+  return (
+    <>
+      {O.isSome(reservationToBeProcessed) ?
+        (
+          <VStack>
+            {reservationToBeProcessed.value.urgent > 0 ?
+              (
+                <Box
+                  py={'10px'}
+                  px={'25px'}
+                  backgroundColor={'#FED7D7'}
+                  borderLeft={'4px'}
+                  borderColor={'#E53E3E'}
+                  minW={'650px'}
+                >
+                  <HStack justifyContent={'space-between'}>
+                    <VStack alignItems={'flex-start'}>
+                      <HStack>
+                        <BsFillExclamationCircleFill color="#E53E3E" size={'24px'} />
+                        <Text fontWeight={'bold'}>Réservations urgentes</Text>
+                      </HStack>
+                      <Text>
+                        Vous avez {reservationToBeProcessed.value.urgent}{' '}
+                        réservations urgentes à traiter
+                      </Text>
+                    </VStack>
+                  </HStack>
+                </Box>
+              ) :
+              null}
+            {reservationToBeProcessed.value.total > 0 ?
+              (
+                <Box
+                  py={'10px'}
+                  px={'25px'}
+                  backgroundColor={'#BEE3F8'}
+                  borderLeft={'4px'}
+                  borderColor={'#3182CE'}
+                  minW={'650px'}
+                >
+                  <HStack justifyContent={'space-between'}>
+                    <VStack alignItems={'flex-start'}>
+                      <HStack>
+                        <BsFillExclamationCircleFill color="#3182CE" size={'24px'} />
+                        <Text fontWeight={'bold'}>Réservations non traitées</Text>
+                      </HStack>
+                      <Text>
+                        Vous avez {reservationToBeProcessed.value.total} réservations à traiter
+                      </Text>
+                    </VStack>
+                    <Button
+                      color={'white'}
+                      backgroundColor={'#3182CE'}
+                      as={Link}
+                      to="/reservation/to-be-processed"
+                      _hover={{ textDecoration: 'none', color: 'white' }}
+                    >
+                      Traiter les réservations
+                    </Button>
+                  </HStack>
+                </Box>
+              ) :
+              null}
+          </VStack>
+        ) :
+        <Spinner />}
+    </>
+  )
+}
