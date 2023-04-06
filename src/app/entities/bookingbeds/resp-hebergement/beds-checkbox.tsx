5b1eff895ec6d55639c5754ca80dd64f01bb65ae
charlesilieff <95299358+charlesilieff@users.noreply.github.com>
2023-04-06 22:38:47 +0200

    checkbox first

--- src/app/entities/bookingbeds/resp-hebergement/beds-checkbox.tsx
+++ src/app/entities/bookingbeds/resp-hebergement/beds-checkbox.tsx
@@ -1,4 +1,4 @@
-import { Radio, RadioGroup, Text, VStack } from '@chakra-ui/react'
+import { Checkbox, Radio, RadioGroup, Text, VStack } from '@chakra-ui/react'
 import * as O from '@effect/data/Option'
 import type { FunctionComponent } from 'react'
 import React from 'react'
@@ -29,16 +29,29 @@ export const IntermittentBeds: FunctionComponent<IProps> = (
               && (!O.exists<string>(b => b === bed.id.toString())(bedId))
 
             return (
-              <Radio
-                key={bed.id}
-                value={bed.id.toString()}
-                isDisabled={isDisabled}
-                isChecked={O.exists<string>(b => b === bed.id.toString())(bedId)}
-              >
-                {`${bed.number} ${bedkind}  (places : ${bed.numberOfPlaces}) ${
-                  isDisabled ? '(réservé)' : ''
-                }`}
-              </Radio>
+              <>
+                <Checkbox
+                  key={bed.id}
+                  isDisabled={isDisabled}
+                  value={bed.id.toString()}
+                  isChecked={O.exists<string>(b => b === bed.id.toString())(bedId)}
+                  onChange={e => selectedBedId(O.some(+e))}
+                >
+                  {`${bed.number} ${bedkind}  (places : ${bed.numberOfPlaces}) ${
+                    isDisabled ? '(réservé)' : ''
+                  }`}
+                </Checkbox>
+                <Radio
+                  key={bed.id}
+                  value={bed.id.toString()}
+                  isDisabled={isDisabled}
+                  isChecked={O.exists<string>(b => b === bed.id.toString())(bedId)}
+                >
+                  {`${bed.number} ${bedkind}  (places : ${bed.numberOfPlaces}) ${
+                    isDisabled ? '(réservé)' : ''
+                  }`}
+                </Radio>
+              </>
             )
           })}
         </VStack>
