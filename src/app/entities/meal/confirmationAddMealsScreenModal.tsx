d41ffdda215ea160dee497ad53e0d82b1b710975
Gurun <gurunlg@hotmail.com>
2023-04-18 11:42:08 +0200

    update merge

--- src/app/entities/meal/confirmationAddMealsScreenModal.tsx
+++ src/app/entities/meal/confirmationAddMealsScreenModal.tsx
@@ -1,11 +1,14 @@
 import {
   Button,
+  Checkbox,
+  HStack,
   Modal,
   ModalBody,
   ModalContent,
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Stack,
   useDisclosure
 } from '@chakra-ui/react'
 import type { IMeal } from 'app/shared/model/meal.model'
@@ -14,28 +17,67 @@ import type { Dayjs } from 'dayjs'
 // import type dayjs from 'dayjs'
 import type { FunctionComponent } from 'react'
 import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
 import { FaBan, FaSave, FaTrash } from 'react-icons/fa'
+
+interface MealSpecial {
+  isSpecialMeal3: boolean
+  // setUpdateBeds: (updateBeds: boolean) => void
+  entity: IMeal[]
+  date: Dayjs
+  numberOfDays: number
+}
 
 export const ConfirmationAddMealsScreenModal: FunctionComponent<
   { mealsData: IMeal[]; date: Dayjs; numberOfDays: number; setDate: (date: Dayjs) => void }
 > = (
   { mealsData, date, numberOfDays, setDate }
 ): JSX.Element => {
+  const {
+    handleSubmit,
+    register
+    // watch,
+    // formState: { errors },
+    // reset: resetForm
+  } = useForm<MealSpecial>()
+
   const [isLoading, setIsLoading] = useState(false)
+  // const [isSpecialMeal, setIsSpecialMeal] = useState<boolean>(false)
+  // const isSpecialMeal2 = useState<boolean>(false)
 
   const apiUrlUpdateMeal = 'api/meals/update'
 
   /**
    * select and update meals of planning on screen
    */
-  const updateMealsOnPeriode = async (entity: IMeal[], date: Dayjs, numberOfDays: number) => {
+  const updateMealsOnPeriode = async (
+    entity: IMeal[],
+    date: Dayjs,
+    numberOfDays: number,
+    isSpecialMeal3: boolean
+  ) => {
+    console.log('entity', entity)
+    console.log('date', date)
+    console.log('isSpecialMeal', isSpecialMeal3)
+
     entity = entity.filter((value, index) => {
       if (index < (numberOfDays)) {
         return value
       }
     }).map((value, index) => {
-      // console.log('value', value)
-      if (index < numberOfDays && value.id !== undefined) {
+      console.log('isSpecialMeal', isSpecialMeal3)
+      if (index < numberOfDays && value.id !== undefined && isSpecialMeal3) {
+        value = {
+          ...value,
+          specialLunch: 1,
+          specialDinner: 1,
+          regularLunch: 0,
+          regularDinner: 0,
+          comment: mealsData[index].comment,
+          breakfast: 1
+        }
+      }
+      if (index < numberOfDays && value.id !== undefined && !isSpecialMeal3) {
         value = {
           ...value,
           specialLunch: 0,
@@ -86,20 +128,47 @@ export const ConfirmationAddMealsScreenModal: FunctionComponent<
           </ModalHeader>
           <ModalBody>
             Êtes-vous sûr de vouloir vous réinscrire aux repas sur la période affichée sur
-            l&apos;écran ?
+            l&apos;écran ? <br />
+            {
+              /* <Checkbox
+              colorScheme={'green'}
+              defaultChecked={true}
+              size={'lg'}
+              id={'specialMeal'}
+              // isSpecialMeal={true}
+            >
+              Repas spéciaux
+            </Checkbox> */
+            }
+            {/* <Checkbox {...setIsSpecialMeal}>Repas spéciaux</Checkbox> */}
+            <Checkbox colorScheme={'green'} defaultChecked={true} {...register('isSpecialMeal3')}>
+              Repas spéciaux
+            </Checkbox>
           </ModalBody>
           <ModalFooter justifyContent={'space-between'}>
             <Button onClick={onClose} leftIcon={<FaBan />} variant="back" isLoading={isLoading}>
               Retour
             </Button>
-            <Button
-              onClick={() => updateMealsOnPeriode(mealsData, date, numberOfDays)}
-              leftIcon={<FaTrash />}
-              variant="danger"
-              isLoading={isLoading}
-            >
-              Confirmer
-            </Button>
+            <Stack>
+              <Button
+                // onSubmit={handleSubmit(handleValidDateAndMealSubmit)}
+                onClick={() => updateMealsOnPeriode(mealsData, date, numberOfDays, true)}
+                // onClick={handleSubmit(updateMealsOnPeriode)}
+                leftIcon={<FaTrash />}
+                variant="danger"
+                isLoading={isLoading}
+              >
+                Confirmer (repas spéciaux)
+              </Button>
+              <Button
+                onClick={() => updateMealsOnPeriode(mealsData, date, numberOfDays, false)}
+                leftIcon={<FaTrash />}
+                variant="danger"
+                isLoading={isLoading}
+              >
+                Confirmer
+              </Button>
+            </Stack>
           </ModalFooter>
         </ModalContent>
       </Modal>
