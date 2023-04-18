d7a0f36d9bc41c9ad4d4578b1d0ab06cbb26ed9a
Gurun <gurunlg@hotmail.com>
2023-04-18 08:35:18 +0200

    update pull request

--- src/app/entities/meal/mealsUserPlanning.tsx
+++ src/app/entities/meal/mealsUserPlanning.tsx
@@ -1,3 +1,4 @@
+import { Box, Grid, Text } from '@chakra-ui/react'
 import type { IMeal } from 'app/shared/model/meal.model'
 import { getDateKey } from 'app/shared/util/date-utils'
 import type { Dayjs } from 'dayjs'
@@ -31,47 +32,134 @@ export const MealsUserPlanning = (
   const positionX = {}
 
   return (
-    <div className="grid-container" style={gridFormatStyle(numberOfDays)}>
-      <div className="timeMyMeals breakfast">Matin</div>
-      <div className="timeMyMeals lunch">Midi</div>
-      <div className="timeMyMeals dinner">Soir</div>
+    <Box>
+      <Grid
+        className="grid-container"
+        borderColor={'#D9D9D9'}
+        overflowX={'scroll'}
+        overflowY={'hidden'}
+      >
+        <Text
+          gridRowStart={4}
+          gridRowEnd={5}
+          gridColumnStart={1}
+          gridColumnEnd={8}
+          textAlign={'center'}
+          overflowWrap={'break-word'}
+          borderTopStyle={'solid'}
+          borderRightStyle={'solid'}
+          borderWidth={'0.15em'}
+          borderColor={'#D9D9D9'}
+          py={2}
+          mt={'-0.1rem'}
+          borderBottom={0}
+          borderLeft={0}
+        >
+          Matin
+        </Text>
+        <Text
+          gridRowStart={5}
+          gridRowEnd={7}
+          gridColumnStart={1}
+          gridColumnEnd={8}
+          textAlign={'center'}
+          overflowWrap={'break-word'}
+          borderTopStyle={'solid'}
+          borderRightStyle={'solid'}
+          borderWidth={'0.15em'}
+          borderColor={'#D9D9D9'}
+          py={2}
+          mt={'-0.1rem'}
+          borderBottom={0}
+          borderLeft={0}
+        >
+          Midi
+        </Text>
+        <Text
+          gridRowStart={7}
+          gridRowEnd={9}
+          gridColumnStart={1}
+          gridColumnEnd={8}
+          textAlign={'center'}
+          overflowWrap={'break-word'}
+          borderTopStyle={'solid'}
+          borderRightStyle={'solid'}
+          borderWidth={'0.15em'}
+          borderColor={'#D9D9D9'}
+          py={2}
+          mt={'-0.1rem'}
+          borderBottom={0}
+          borderLeft={0}
+        >
+          Soir
+        </Text>
+        <Months date={date} month={0} totalDays={totalDays}></Months>
+        {date.date() + numberOfDays - 1 > totalDays && (
+          <Months date={date} month={1} totalDays={totalDays}></Months>
+        )}
 
-      <Months date={date} month={0} totalDays={totalDays}></Months>
+        {monthDays.map((_, index) => {
+          const gridColumnStart = 8 + index
+          // On construit l'objet qui va permettre de récuperer la bonne position en X pour afficher les réservations.
+          const dateDay = date.add(index, 'day')
+          const dateKey = getDateKey(dateDay)
+          positionX[dateKey] = gridColumnStart
+          return (
+            <Day
+              positionX={gridColumnStart}
+              key={dateKey}
+              date={dateDay}
+              index={index}
+              mealsData={mealsData}
+            />
+          )
+        })}
+      </Grid>
 
-      {date.date() + numberOfDays - 1 > totalDays && (
-        <Months date={date} month={1} totalDays={totalDays}></Months>
-      )}
+      {
+        /* <div className="grid-container" style={gridFormatStyle(numberOfDays)}>
+        <div className="timeMyMeals breakfast">Matin</div>
+        <div className="timeMyMeals lunch">Midi</div>
+        <div className="timeMyMeals dinner">Soir</div>
 
-      {monthDays.map((_, index) => {
-        const gridColumnStart = 8 + index
-        // On construit l'objet qui va permettre de récuperer la bonne position en X pour afficher les réservations.
-        const dateDay = date.add(index, 'day')
-        const dateKey = getDateKey(dateDay)
-        positionX[dateKey] = gridColumnStart
-        return (
-          <Day
-            positionX={gridColumnStart}
-            key={dateKey}
-            date={dateDay}
-            index={index}
-            mealsData={mealsData}
-          />
-        )
-      })}
-    </div>
+        <Months date={date} month={0} totalDays={totalDays}></Months>
+
+        {date.date() + numberOfDays - 1 > totalDays && (
+          <Months date={date} month={1} totalDays={totalDays}></Months>
+        )}
+
+        {monthDays.map((_, index) => {
+          const gridColumnStart = 8 + index
+          // On construit l'objet qui va permettre de récuperer la bonne position en X pour afficher les réservations.
+          const dateDay = date.add(index, 'day')
+          const dateKey = getDateKey(dateDay)
+          positionX[dateKey] = gridColumnStart
+          return (
+            <Day
+              positionX={gridColumnStart}
+              key={dateKey}
+              date={dateDay}
+              index={index}
+              mealsData={mealsData}
+            />
+          )
+        })}
+      </div> */
+      }
+    </Box>
   )
 }
 
-/**
- * Style of the grid according to the number of days.
- * @param numberOfDays
- * @returns
- */
-function gridFormatStyle(numberOfDays: number) {
-  const style = {} as React.CSSProperties
-  if (numberOfDays === 15) {
-    style.borderLeftWidth = '0.2em'
-    style.gridTemplateColumns = '3em repeat(21, minmax(20px, 1fr))'
-  }
-  return style
-}
+// /**
+//  * Style of the grid according to the number of days.
+//  * @param numberOfDays
+//  * @returns
+//  */
+// function gridFormatStyle(numberOfDays: number) {
+//   const style = {} as React.CSSProperties
+//   if (numberOfDays === 15) {
+//     style.borderLeftWidth = '0.2em'
+//     style.gridTemplateColumns = '3em repeat(21, minmax(20px, 1fr))'
+//   }
+//   return style
+// }
