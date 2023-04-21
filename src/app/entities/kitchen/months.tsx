c7bd70829354ad62a0accca8824cc16fc3fea4fc
charlesilieff <95299358+charlesilieff@users.noreply.github.com>
2023-04-21 14:27:00 +0200

    Update months.tsx

--- src/app/entities/kitchen/months.tsx
+++ src/app/entities/kitchen/months.tsx
@@ -13,7 +13,7 @@ interface IProps {
 export const Months: FunctionComponent<IProps> = ({ month, date, totalDays, numberOfDays }) => {
   const offSet = numberOfDays === 7 ? 6 : 8
 
-  // const endTable = totalDays + offSet
+
   const endTable = numberOfDays + offSet
   // Le jour de la date passé en paramétre.
   const today = date.date()
@@ -23,27 +23,17 @@ export const Months: FunctionComponent<IProps> = ({ month, date, totalDays, numb
 
   // Position de début vertical des cases. Si c'est le deuxième mois (month = 1), on ajoute les jours du mois précédent.
   const gridColumnStart = offSet + month * remainingDays
-  // const gridColumnStart = month === 0 ?
-  // offSet + month * remainingDays :
-  // offSet + month * remainingDays + 1
+  
 
   // Position de fin vertical des cases. Si c'est le deuxième mois (month = 1), la postion est absolu à la fin (endTable)
-  // const gridColumnEnd = month === 0 ? offSet + remainingDays : endTable
-  // const gridColumnEnd = endTable
+  
   const gridColumnEnd = remainingDays < 4 ? offSet + remainingDays : endTable
 
-  // console.log('gridColumnStart Months:', gridColumnStart)
-  // console.log('remainingDays Months:', remainingDays)
 
-  // const style = {
-  //   gridColumnStart,
-  //   gridColumnEnd
-  // } as React.CSSProperties
+ 
 
   return (
-    // <div className="month" style={style}>
-    //   {date.add(month, 'month').format('MMMM')}a
-    // </div>
+   
     <Text
       className="month"
       gridColumnEnd={gridColumnEnd}
@@ -53,7 +43,6 @@ export const Months: FunctionComponent<IProps> = ({ month, date, totalDays, numb
       borderColor={'#D9D9D9'}
       borderBottomWidth={'0.1em'}
       borderLeftWidth={'0.2em'}
-      // borderRightWidth={'0.2em'}
       textTransform={'capitalize'}
       fontWeight={'bold'}
       backgroundColor={'white'}
